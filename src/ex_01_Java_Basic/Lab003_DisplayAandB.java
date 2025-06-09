04e467d7a5356b4ee2c4a4301f29e813382274cb
Gayathri .S.Y <gayathriyathish12@gmail.com>
2025-06-09 15:01:03 +0530

    init commit

---
+++ src/ex_01_Java_Basic/Lab003_DisplayAandB.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab003_DisplayAandB {
+    public static void main(String[]args)
+    {
+        System.out.println("A");
+        System.out.println("B");
+    }
+}
